c39cec155139b3147e125494f924635f7d798827
Hassan Raja <hassan.uog001@gmail.com>
2019-09-13 15:09:01 +0500

    webpack setup for scss and js

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+// Webpack uses this to work with directories
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// This is main configuration object
+// Here you write different options and tell Webpack what to do
+
+module.exports = {
+  // Path to your entry point. From this file webpack will begin his work
+  entry: "./src/javascript/index.js",
+
+  // Path and filename of your result bundle.
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        // Apply rule for .scss, .sass or .css files
+        test: /\.(sa|sc|c)ss$/,
+        // Set loaders to transform files.
+        // Loaders are applying from right to left(!)
+        // The first loader will be applied after others
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            // This loader resolves url() and @imports inside css
+            loader: "css-loader"
+          },
+          {
+            // Then we apply postCSS fixes like autoprefixer and minifying
+            loader: "postcss-loader"
+          },
+          {
+            // Fist we transform SASS to standard CSS
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass")
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "bundle.css"
+    })
+  ],
+  // Default mode for Webpack is production.
+  // Depending on mode Webpcak will apply different things on final bundle. For now we don't need productions' JavaScript minifying and other thing so let's set mode to development
+  mode: "development"
+};
